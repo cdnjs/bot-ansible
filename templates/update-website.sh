5e3d39e3f4af770df99abc2005e342617519c318
Sven Sauleau <sven@cloudflare.com>
2020-01-01 20:48:42 +0000

    update

--- templates/update-website.sh
+++ templates/update-website.sh
@@ -1,4 +1,4 @@
-set -x
+set -xe
 
 # Generate package.min.json and store it
 cd {{ bot_base_path }}/cdnjs
@@ -8,10 +8,19 @@ packages set < /tmp/out.json
 # Wait until gcp storage is okay (FIXME)
 sleep 300
 
-# Restart website and api to use the new package.min.js
-heroku dyno:restart --app cdnjs-new-website2
-heroku dyno:restart --app cdnjs-api2
+cd {{ bot_base_path }}/new-website
+if [[ $(git status --porcelain ./sri | wc -l) -gt 0 ]]; then
+    # The package generation has likely added new SRIs; commit them, if any
+    git add ./sri
+    git commit -am "update SRIs"
+    git push origin master
+    git push heroku-api master
+    git push heroku-web master
+else
+    # Else just restart the apps to pickup the new package.min.json
+    heroku dyno:restart --app cdnjs-new-website2
+    heroku dyno:restart --app cdnjs-api2
+fi
 
 # Update Algolia index (website search)
-cd {{ bot_base_path }}/new-website
 algolia update
