f300255895f42e6f0fcebfd91a0d581324cec87c
Sven Sauleau <sven@cloudflare.com>
2020-02-03 16:18:53 +0000

    use the new sri submodule

--- templates/update-website.sh
+++ templates/update-website.sh
@@ -5,19 +5,32 @@ cd {{ bot_base_path }}/cdnjs
 packages generate > /tmp/out.json
 packages set < /tmp/out.json
 
+cd {{ bot_base_path }}/SRIs
+
+# If there are any changes to SRIs
+CHANGED=$(git status --porcelain)
+if [ -n "${CHANGED}" ]; then
+    git add .
+    git commit -am "update SRIs"
+    git push origin master
+fi
+
 # Wait until gcp storage is okay (FIXME)
 sleep 300
 
+# Go to website
 cd {{ bot_base_path }}/new-website
-if [[ $(git status --porcelain ./sri | wc -l) -gt 0 ]]; then
-    # The package generation has likely added new SRIs; commit them, if any
-    git add ./sri
-    git commit -am "update SRIs"
-    git push origin master
+
+# Check if there are changes from upstream
+if ! git diff --quiet remotes/origin/HEAD; then
+    # Pull for new modifications
+    git pull origin master --rebase
+
+    # Push them to heroku, which will also trigger a restart
     git push heroku-api master
     git push heroku-web master
 else
-    # Else just restart the apps to pickup the new package.min.json
+    # Restart apps to pick up new eventual changes
     heroku dyno:restart --app cdnjs-new-website2
     heroku dyno:restart --app cdnjs-api2
 fi
