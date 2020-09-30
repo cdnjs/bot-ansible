3e7a7f50ad2a89636becacfc3780b8ab13c81ee9
Sven Sauleau <sven@cloudflare.com>
2020-09-30 16:32:16 +0100

    remove heroku autoupdate

--- templates/update-website.sh
+++ templates/update-website.sh
@@ -18,22 +18,3 @@ if [ -n "${CHANGED}" ]; then
     git commit -am "update SRIs"
     git push origin master
 fi
-
-# Wait until gcp storage is okay (FIXME)
-sleep 300
-
-# Go to website
-cd {{ bot_base_path }}/new-website
-
-# Check if there are changes from upstream
-if ! git diff --quiet remotes/origin/HEAD; then
-    # Pull for new modifications
-    git pull origin master --rebase
-    git submodule update --remote --recursive
-
-    # Push them to heroku, which will also trigger a restart
-    git push heroku-web master
-else
-    # Restart apps to pick up new eventual changes
-    heroku dyno:restart --app cdnjs-new-website2
-fi
